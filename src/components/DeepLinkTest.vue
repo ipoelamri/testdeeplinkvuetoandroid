613f7ba07419ad83b351d0a9a5d27406c481c839
ipoelamri <ipoelamri94@gmail.com>
2025-08-31 12:06:27 +0700

    test2

--- src/components/DeepLinkTest.vue
+++ src/components/DeepLinkTest.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="text-center">
     <h2>Tekan tombol di bawah untuk membuka aplikasi</h2>
     <button @click="openApp" class="btn">Buka Aplikasi Saya</button>
   </div>
@@ -10,22 +10,11 @@ export default {
   name: "OpenAppButton", // Nama komponen Vue
   methods: {
     openApp() {
-      // 1. Definisikan URL scheme untuk aplikasi Anda.
-      // Ganti 'myappb://' dengan URL scheme yang sesuai dengan konfigurasi AndroidManifest.xml Anda.
       const appUrl = "mydlapp://";
-
-      // 2. Definisikan URL Play Store untuk fallback.
-      // Ganti 'com.contoh.aplikasi' dengan package name aplikasi Anda di Play Store.
       const playStoreUrl =
         "https://play.google.com/store/apps/details?id=com.contoh.aplikasi";
-
-      // 3. Coba buka aplikasi dengan URL scheme.
       window.location.href = appUrl;
 
-      // 4. Tambahkan fallback.
-      // Gunakan setTimeout agar ada jeda waktu (misalnya 500ms) untuk
-      // memberikan kesempatan pada perangkat untuk membuka aplikasi.
-      // Jika aplikasi tidak terbuka, browser akan melanjutkan ke Play Store.
       setTimeout(() => {
         window.location.href = playStoreUrl;
       }, 500);
@@ -35,7 +24,6 @@ export default {
 </script>
 
 <style scoped>
-/* Contoh styling sederhana untuk tombol */
 .btn {
   background-color: #42b983; /* Warna hijau khas Vue.js */
   color: white;
